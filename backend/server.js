428c7d50cda70feb24889d832618fabb9b326fcc
hajarsk <hajar@kekwaaa.local>
2025-03-13 00:48:20 +0800

    MongoDB Atlas & Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -17,9 +18,17 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts',workoutRoutes)
 
-// listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+   .then(() => {
+     // listen for request
+    app.listen(process.env.PORT, () => {
+    console.log('connected to db and listening on port', process.env.PORT)
 })
+   })
+   .catch((error) => {
+    console.log(error)
+   })
 
-process.env
+
+
