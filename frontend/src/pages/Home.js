5255ff8254a7e66bd299414bfb36144e738c7efa
hajarsk <hajar@kekwaaa.local>
2025-03-14 03:51:28 +0800

    create workout form

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,35 +1,36 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 // components
-import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+  const [workouts, setWorkouts] = useState(null);
 
-    useEffect(() => {
+  useEffect(() => {
+    const fethWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
 
-        const fethWorkouts = async () => {
-            const response = await fetch('/api/workouts')
-            const json = await response.json()
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
 
+    fethWorkouts();
+  }, []);
 
-        if (response.ok) {
-            setWorkouts(json)
-        }
-        }
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+};
 
-        fethWorkouts()
-    }, [])
-
-    return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout._id} workout= {workout} />
-                ))}
-            </div>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
